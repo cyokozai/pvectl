fafaf2b261a4e5b3c280120a8803b0480bee9a1f
cyokozai <yuu4suke27@outlook.jp>
2025-08-20 06:13:31 +0900

    refactor: MainCommand関数をサブコマンドの定義に置き換え、SubCommands構造体を追加

--- app/options/commands.go
+++ app/options/commands.go
@@ -1,43 +1,32 @@
 package options
 
-
 import (
 	// "bufio"
 	"fmt"
 	"log"
-    "github.com/cyokozai/pvectl/app/cli"
+	"github.com/cyokozai/pvectl/app/cli"
 )
 
-
 // MainCommand function: prints "Hello World" to standard output
-func MainCommand(args []string, inout *cli.InOut) int {
-	options, err := OptionParser(args, inout) // Parse command line options
-	if err != nil {
-		fmt.Fprintf(inout.StdErr, "Error: %v\n", err) 	// Print error to standard error
-		log.Println("Error parsing options:", err)		// Log the error if any
-		
-		return 1
-	}
+var MainCommand = cli.Command("pvectl", []SubCommands{
+	{
+		Name:        "foo",
+		Description: "foo command",
+		Run: func(args []string, inout *cli.InOut) int {
+			fmt.Fprintf(inout.StdOut, "foo command executed successfully\n")
+			log.Println("foo command executed successfully")
 
-	if options.Help {
-		return 0
-	}
+			return 0
+		},
+	},
+	{
+		Name:        "bar",
+		Description: "bar command",
+		Run: func(args []string, inout *cli.InOut) int {
+			fmt.Fprintf(inout.StdOut, "bar command executed successfully\n")
+			log.Println("bar command executed successfully")
 
-
-	if err := MainCommandByOptions(options, inout); err != nil {
-		fmt.Fprintf(inout.StdErr, "Error: %v\n", err)  				 // Print error to standard error
-		log.Println("Error executing main command by options:", err) // Log the error if any
-		
-		return 1
-	}
-
-	return 0
-}
-
-
-// MainCommandByOptions function: executes the main command with the given options
-func MainCommandByOptions(options *Options, inout *cli.InOut) error {
-	fmt.Fprintf(inout.StdOut, "pvectl executed successfully\n")
-
-	return nil
-}
+			return 0
+		},
+	},
+})
