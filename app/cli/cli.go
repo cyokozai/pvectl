186e49e02cb4785ab5bc58d4f587d21bda8da270
cyokozai <yuu4suke27@outlook.jp>
2025-08-24 04:34:35 +0900

    feat: FlagAnalyzerとFlagParser関数を追加し、コマンドラインフラグの解析と補完機能を実装

--- app/cli/cli.go
+++ app/cli/cli.go
@@ -72,33 +72,100 @@ type Flag struct {
 
 // FlagAnalyzer function: analyzes the flags in the given options struct
 func FlagAnalyzer(options any) []Flag {
-	flags := make([]Flag, 0)
-	t 	  := reflect.TypeOf(options).Elem()
-	m 	  := t.NumField()
+	flags  := make([]Flag, 0)
+	typeOf := reflect.TypeOf(options).Elem()
+	m 	   := typeOf.NumField()
 
 	for i := 0; i < m; i++ {
-		field := t.Field(i)
-		name  := field.Tag.Get("flag")
-		desc  := field.Tag.Get("description")
-
+		field := typeOf.Field(i)
+		
 		f := Flag{
 			FlagName:    field.Name,
-			Name:        name,
-			Description: desc,
+			Name:        field.Tag.Get("name"),
+			Description: field.Tag.Get("description"),
 		}
-
 		switch field.Type.Kind() {
 		case reflect.String:
-			f = "string"
+			f.Type = "string"
 		case reflect.Bool:
-			f = "bool"
+			f.Type = "bool"
 		case reflect.Int:
-			f = "int"
+			f.Type = "int"
 		default:
-			panic("unhandled default case")
+			log.Fatal("unhandled flag type: ", field.Type.Kind())
+			panic("unhandled flag type: " + field.Type.Kind().String())
 		}
+
 		flags = append(flags, f)
 	}
 
 	return flags
+}
+
+
+// FlagParser function: parses command-line flags
+func FlagParser(name string, args []string, options any) error {
+	fs 	  := FlagAnalyzer(options)
+	flags := flag.NewFlagSet(name, flag.ContinueOnError)
+	
+	for _, fl := range fs {
+		f := reflect.ValueOf(options).Elem().FieldByName(fl.FlagName)
+
+		switch fl.Type {
+		case "string":
+			flags.StringVar(f.Addr().Interface().(*string), fl.Name, f.String(), fl.Description)
+		case "bool":
+			flags.BoolVar(f.Addr().Interface().(*bool), fl.Name, f.Bool(), fl.Description)
+		case "int":
+			flags.IntVar(f.Addr().Interface().(*int), fl.Name, int(f.Int()), fl.Description)
+		}
+	}
+
+	return flags.Parse(args)
+}
+
+
+// CommandCompletion function: wraps a command with completion support
+func CommandCompletion(c Commands, compf func(args []string) []string) Commands {
+	return func(args []string, inout *InOut) int {
+		if inout.Env["GO_FLAGS_COMPLETION"] == "1" {
+			completions := compf(args)
+			fmt.Fprintln(inout.StdOut, strings.Join(completions, "\n"))
+			
+			return 0
+		}
+
+		return c(args, inout)
+	}
+}
+
+
+// CompletionByFlags function: creates a completion function based on the provided flags
+func CompletionByFlags(fs []Flag) func(args []string) []string {
+	return func(args []string) []string {
+		return Completion(args, fs)
+	}
+}
+
+
+// Completion function: generates possible completions based on the current arguments and available flags
+func Completion(args []string, fs []Flag) []string {
+	comps := []string{}
+	if len(args) == 0 {
+		for _, flag := range fs {
+			comps = append(comps, "--" + flag.Name)
+		}
+
+		return comps
+	}
+
+	last := args[len(args) - 1]
+	
+	for _, flag := range fs {
+		if strings.HasPrefix("--" + flag.Name, last) || strings.HasPrefix("-" + flag.Name, last) {
+			comps = append(comps, "--" + flag.Name)
+		}
+	}
+
+	return comps
 }
